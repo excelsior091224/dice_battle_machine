59f5d1dad38d9a8ba44d6f6557bcce78117536a9
excelsior091224 <44745901+excelsior091224@users.noreply.github.com>
2019-08-04 20:13:46 +0900

    Update dice_battle_machine.py
    ・試合結果ログを保管するためのfight_logディレクトリを作るようにした
    ・試合結果がtxtファイルとして保存されるようにした
    ・その他微修正

--- dice_battle_machine.py
+++ dice_battle_machine.py
@@ -1,7 +1,16 @@
-import random, re, math
+import random, re, math, os
+from datetime import datetime
 
 def start():
     while True:
+        global fight_log_dir
+        fight_log_dir = "fight_log"
+
+        try:
+            os.makedirs(fight_log_dir)
+        except FileExistsError:
+            pass
+
         s = input("試合を開始する場合「試合開始」と入力してください:")
         if s == "試合開始":
             print("試合を開始します")
@@ -123,6 +132,14 @@ def dice(a, b):
     return dice_roll
 
 def fight():
+    fight_datetime = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    log_text_title = fight_log_dir + '/' + fight_datetime + '_' + red_name + 'vs' + blue_name + '.txt'
+
+    log_text = open(log_text_title, 'w')
+    log_text.write(title + '\n' + '試合開始\n' + '\n')
+    log_text.close()
+
     i = hp / down_hp
 
     red_hp = hp
@@ -131,6 +148,8 @@ def fight():
     current_turn = 1
     blue_down = i - 1
     red_down = i - 1
+    red_down_count = 0
+    blue_down_count = 0
 
     while current_round <= round_num or red_hp <= 0 or blue_hp <= 0:
         turn_call = input("「ラウンド数-ターン数」を入力してください。3ターンで1ラウンド経過です。例:1ラウンド1回目のダイスの場合:1-1:")
@@ -141,43 +160,136 @@ def fight():
             red_dice_sum = sum(red_dice)
             blue_dice_sum = sum(blue_dice)
 
+            round_turn = str(current_round) + "ラウンド" + str(current_turn) + "ターン目"
+
+            round_end = str(current_round) + "ラウンド終了。"
+            all_round_end = "全ラウンド終了。判定に入ります。"
+
+            red_judgment_win = red_name + "選手の判定勝利。"
+            blue_judgment_win = blue_name + "選手の判定勝利。"
+            judgment_draw = "本試合、引き分けで終了。"
+
             if red_dice_sum > blue_dice_sum:
                 before_blue_hp = blue_hp
                 damage = red_dice_sum - blue_dice_sum
                 blue_hp -= damage
+                blue_damage_result = blue_name + "選手に" + str(damage) + "のダメージ"
+                dice_result = "赤コーナー:" + red_name + ":" + str(red_dice) + "=" + str(red_dice_sum) + " " + "青コーナー:" + blue_name + ":" + str(blue_dice) + "=" + str(blue_dice_sum)
+                hp_result = "赤コーナー:" + red_name + ":HP" + str(red_hp) + " vs " + "青コーナー:" + blue_name + ":HP" + str(blue_hp)
                 print(title)
-                print(str(current_round) + "ラウンド" + str(current_turn) + "ターン目")
-                print("赤コーナー:" + red_name + ":" + str(red_dice) + "=" + str(red_dice_sum) + " " + "青コーナー:" + blue_name + ":" + str(blue_dice) + "=" + str(blue_dice_sum))
-                print(blue_name + "選手に" + str(damage) + "のダメージ")
-                print("赤コーナー:" + red_name + ":HP" + str(red_hp) + " vs " + "青コーナー:" + blue_name + ":HP" + str(blue_hp))
+                print(round_turn)
+                print(dice_result)
+                print(blue_damage_result)
+                print(hp_result)
+
+                log_text = open(log_text_title, 'a')
+                log_text.write(
+                round_turn + '\n' +
+                dice_result + '\n' +
+                blue_damage_result + '\n' +
+                hp_result + '\n'
+                )
+                log_text.close()
 
                 if blue_hp != hp and blue_hp < (blue_down * down_hp + 1) and (before_blue_hp > blue_down * down_hp):
-                    print(blue_name + "選手1回ダウン。")
+                    blue_down_count += 1
+                    blue_down_result = blue_name + "選手、この試合通算" + str(blue_down_count) + "回目のダウン。"
+                    print(blue_down_result)
                     if blue_hp > 0 and blue_hp % down_hp == 0:
                         blue_down = (blue_hp / down_hp) - 1
-                        print("次HP" + str(int(blue_down * down_hp)) + "を切ったらダウン。")
+                        blue_next_down = "次HP" + str(int(blue_down * down_hp)) + "を切ったらダウン。"
+                        print(blue_next_down)
+                        log_text = open(log_text_title, 'a')
+                        log_text.write(
+                        blue_down_result + '\n' +
+                        blue_next_down + '\n' + '\n'
+                        )
+                        log_text.close()
                     elif blue_hp > 0 and blue_hp % down_hp != 0:
                         blue_down = math.floor(blue_hp / down_hp)
-                        print("次HP" + str(int(blue_down * down_hp)) + "を切ったらダウン。")
+                        blue_next_down = "次HP" + str(int(blue_down * down_hp)) + "を切ったらダウン。"
+                        print(blue_next_down)
+                        log_text = open(log_text_title, 'a')
+                        log_text.write(
+                        blue_down_result + '\n' +
+                        blue_next_down + '\n' + '\n'
+                        )
+                        log_text.close()
+                    elif blue_hp <= 0:
+                        blue_ko = blue_name + "選手KO。" + red_name + "選手の勝利。"
+                        print(blue_ko)
+                        log_text = open(log_text_title, 'a')
+                        log_text.write(
+                        blue_down_result + '\n' +
+                        blue_ko
+                        )
+                        log_text.close()
+                        break
+                else:
+                    log_text = open(log_text_title, 'a')
+                    log_text.write( '\n')
+                    log_text.close()
 
             elif blue_dice_sum > red_dice_sum:
                 before_red_hp = red_hp
                 damage = blue_dice_sum - red_dice_sum
                 red_hp -= damage
+                red_damage_result = red_name + "選手に" + str(damage) + "のダメージ"
+                dice_result = "赤コーナー:" + red_name + ":" + str(red_dice) + "=" + str(red_dice_sum) + " " + "青コーナー:" + blue_name + ":" + str(blue_dice) + "=" + str(blue_dice_sum)
+                hp_result = "赤コーナー:" + red_name + ":HP" + str(red_hp) + " vs " + "青コーナー:" + blue_name + ":HP" + str(blue_hp)
                 print(title)
-                print(str(current_round) + "ラウンド" + str(current_turn) + "ターン目")
-                print("赤コーナー:" + red_name + ":" + str(red_dice) + "=" + str(red_dice_sum) + " " + "青コーナー:" + blue_name + ":" + str(blue_dice) + "=" + str(blue_dice_sum))
-                print(red_name + "選手に" + str(damage) + "のダメージ")
-                print("赤コーナー:" + red_name + ":HP" + str(red_hp) + " vs " + "青コーナー:" + blue_name + ":HP" + str(blue_hp))
+                print(round_turn)
+                print(dice_result)
+                print(red_damage_result)
+                print(hp_result)
+
+                log_text = open(log_text_title, 'a')
+                log_text.write(
+                round_turn + '\n' +
+                dice_result + '\n' +
+                red_damage_result + '\n' +
+                hp_result + '\n'
+                )
+                log_text.close()
 
                 if red_hp != hp and red_hp < (red_down * down_hp + 1) and (before_red_hp > red_down * down_hp):
-                    print(red_name + "選手1回ダウン。")
+                    red_down_count += 1
+                    red_down_result = red_name + "選手、この試合通算" + str(red_down_count) + "回目のダウン。"
+                    print(red_down_result)
                     if red_hp > 0 and red_hp % down_hp == 0:
                         red_down = (red_hp / down_hp) - 1
-                        print("次HP" + str(int(red_down * down_hp)) + "を切ったらダウン。")
+                        red_next_down = "次HP" + str(int(red_down * down_hp)) + "を切ったらダウン。"
+                        print(red_next_down)
+                        log_text = open(log_text_title, 'a')
+                        log_text.write(
+                        red_down_result + '\n' +
+                        red_next_down + '\n' + '\n'
+                        )
+                        log_text.close()
                     elif red_hp > 0 and red_hp % down_hp != 0:
                         red_down = math.floor(red_hp / down_hp)
-                        print("次HP" + str(int(red_down * down_hp)) + "を切ったらダウン。")
+                        red_next_down = "次HP" + str(int(red_down * down_hp)) + "を切ったらダウン。"
+                        print(red_next_down)
+                        log_text = open(log_text_title, 'a')
+                        log_text.write(
+                        red_down_result + '\n' +
+                        red_next_down + '\n' + '\n'
+                        )
+                        log_text.close()
+                    elif red_hp <= 0:
+                        red_ko = red_name + "選手KO。" + blue_name + "選手の勝利。"
+                        print(red_ko)
+                        log_text = open(log_text_title, 'a')
+                        log_text.write(
+                        red_down_result + '\n' +
+                        red_ko
+                        )
+                        log_text.close()
+                        break
+                else:
+                    log_text = open(log_text_title, 'a')
+                    log_text.write( '\n')
+                    log_text.close()
 
             else:
                 before_red_hp = red_hp
@@ -188,63 +300,152 @@ def fight():
                     red_hp += heal
                 if blue_hp < hp:
                     blue_hp += heal
+
+                dice_result = "赤コーナー:" + red_name + ":" + str(red_dice) + "=" + str(red_dice_sum) + " " + "青コーナー:" + blue_name + ":" + str(blue_dice) + "=" + str(blue_dice_sum)
+                hp_result = "赤コーナー:" + red_name + ":HP" + str(red_hp) + " vs " + "青コーナー:" + blue_name + ":HP" + str(blue_hp)
+                red_heal = red_name + "選手HP1回復。"
+                blue_heal = blue_name + "選手HP1回復。"
+                both_heal = "両者HP1回復。"
+                turn_elapsed = "1ターン経過。"
+
                 print(title)
-                print(str(current_round) + "ラウンド" + str(current_turn) + "ターン目")
-                print("赤コーナー:" + red_name + ":" + str(red_dice) + "=" + str(red_dice_sum) + " " + "青コーナー:" + blue_name + ":" + str(blue_dice) + "=" + str(blue_dice_sum))
+                print(round_turn)
+                print(dice_result)
+
+                log_text = open(log_text_title, 'a')
+                log_text.write(
+                round_turn + '\n' +
+                dice_result + '\n'
+                )
+                log_text.close()
+
                 if before_red_hp < hp and before_blue_hp == hp:
-                    print(red_name + "選手HP1回復。")
+                    print(red_heal)
+                    log_text = open(log_text_title, 'a')
+                    log_text.write(
+                    red_heal + '\n'
+                    )
+                    log_text.close()
                 elif before_blue_hp < hp and before_red_hp == hp:
-                    print(blue_name + "選手HP1回復。")
+                    print(blue_heal)
+                    log_text = open(log_text_title, 'a')
+                    log_text.write(
+                    blue_heal + '\n'
+                    )
+                    log_text.close()
                 elif before_red_hp < hp and before_blue_hp < hp:
-                    print("両者HP1回復。")
+                    print(both_heal)
+                    log_text = open(log_text_title, 'a')
+                    log_text.write(
+                    both_heal + '\n'
+                    )
+                    log_text.close()
                 else:
-                    print("1ターン経過。")
-                print("赤コーナー:" + red_name + ":HP" + str(red_hp) + " vs " + "青コーナー:" + blue_name + ":HP" + str(blue_hp))
+                    print(turn_elapsed)
+                    log_text = open(log_text_title, 'a')
+                    log_text.write(
+                    turn_elapsed + '\n'
+                    )
+                    log_text.close()
+
+                print(hp_result)
+
+                log_text = open(log_text_title, 'a')
+                log_text.write(
+                hp_result + '\n'
+                )
+                log_text.close()
+
                 if red_hp > 0 and red_hp % down_hp == 0:
                     red_down = (red_hp / down_hp) - 1
-                    print("赤コーナー:" + red_name + "選手、次HP" + str(int(red_down * down_hp)) + "を切ったらダウン。")
+                    red_next_down = "赤コーナー:" + red_name + "選手、次HP" + str(int(red_down * down_hp)) + "を切ったらダウン。"
+                    print(red_next_down)
+                    log_text = open(log_text_title, 'a')
+                    log_text.write(
+                    red_next_down + '\n'
+                    )
+                    log_text.close()
                 elif red_hp > 0 and red_hp % down_hp != 0:
                     red_down = math.floor(red_hp / down_hp)
-                    print("赤コーナー:" + red_name + "選手、次HP" + str(int(red_down * down_hp)) + "を切ったらダウン。")
+                    red_next_down = "赤コーナー:" + red_name + "選手、次HP" + str(int(red_down * down_hp)) + "を切ったらダウン。"
+                    print(red_next_down)
+                    log_text = open(log_text_title, 'a')
+                    log_text.write(
+                    red_next_down + '\n'
+                    )
+                    log_text.close()
                 if blue_hp > 0 and blue_hp % down_hp == 0:
                     blue_down = (blue_hp / down_hp) - 1
-                    print("青コーナー:" + blue_name + "選手、次HP" + str(int(blue_down * down_hp)) + "を切ったらダウン。")
+                    blue_next_down = "青コーナー:" + blue_name + "選手、次HP" + str(int(blue_down * down_hp)) + "を切ったらダウン。"
+                    print(blue_next_down)
+                    log_text = open(log_text_title, 'a')
+                    log_text.write(
+                    blue_next_down + '\n' + '\n'
+                    )
+                    log_text.close()
                 elif blue_hp > 0 and blue_hp % down_hp != 0:
                     blue_down = math.floor(blue_hp / down_hp)
-                    print("青コーナー:" + blue_name + "選手、次HP" + str(int(blue_down * down_hp)) + "を切ったらダウン。")
-
-
-            if red_hp <= 0:
-                print(red_name + "選手KO。" + blue_name + "選手の勝利。")
-                break
-            elif blue_hp <= 0:
-                print(blue_name + "選手KO。" + red_name + "選手の勝利。")
-                break
+                    blue_next_down = "青コーナー:" + blue_name + "選手、次HP" + str(int(blue_down * down_hp)) + "を切ったらダウン。"
+                    print(blue_next_down)
+                    log_text = open(log_text_title, 'a')
+                    log_text.write(
+                    blue_next_down + '\n' + '\n'
+                    )
+                    log_text.close()
 
             current_turn += 1
 
             if current_turn > 3:
-                print(str(current_round) + "ラウンド終了。")
+                print(round_end)
+                log_text = open(log_text_title, 'a')
+                log_text.write(
+                round_end + '\n' + '\n'
+                )
+                log_text.close()
                 current_round += 1
                 current_turn = 1
 
             if current_round > round_num:
-                print("全ラウンド終了。判定に入ります。")
+                print(all_round_end)
+
+                log_text = open(log_text_title, 'a')
+                log_text.write(
+                all_round_end + '\n' + '\n'
+                )
+                log_text.close()
 
                 if red_hp > blue_hp:
                     print(title)
-                    print("赤コーナー:" + red_name + ":HP" + str(red_hp) + " vs " + "青コーナー:" + blue_name + ":HP" + str(blue_hp))
-                    print(red_name + "選手の判定勝利。")
+                    print(hp_result)
+                    print(red_judgment_win)
+                    log_text = open(log_text_title, 'a')
+                    log_text.write(
+                    hp_result + '\n' +
+                    red_judgment_win
+                    )
+                    log_text.close()
                     break
                 elif blue_hp > red_hp:
                     print(title)
-                    print("赤コーナー:" + red_name + ":HP" + str(red_hp) + " vs " + "青コーナー:" + blue_name + ":HP" + str(blue_hp))
-                    print(blue_name + "選手の判定勝利。")
+                    print(hp_result)
+                    print(blue_judgment_win)
+                    log_text = open(log_text_title, 'a')
+                    log_text.write(
+                    hp_result + '\n' +
+                    blue_judgment_win
+                    )
+                    log_text.close()
                     break
                 else:
                     print(title)
-                    print("赤コーナー:" + red_name + ":HP" + str(red_hp) + " vs " + "青コーナー:" + blue_name + ":HP" + str(blue_hp))
-                    print("本試合、引き分けで終了。")
+                    print(hp_result)
+                    print(judgment_draw)
+                    log_text = open(log_text_title, 'a')
+                    log_text.write(
+                    hp_result + '\n' +
+                    judgment_draw
+                    )
+                    log_text.close()
                     break
 
         else:
